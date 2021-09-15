65d6ff28f13a0e72f517b046460d764b082abbee
Manish Hazra <64765035+Manish-Hazra@users.noreply.github.com>
2021-09-15 16:27:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const http = require('http');
+const fs = require('fs');
+
+const handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./assets/html/index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+http.createServer(handleRequest).listen(8000);
+
