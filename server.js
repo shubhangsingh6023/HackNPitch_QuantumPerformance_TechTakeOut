d5d3e00ddc262d249bf256d0156dcb4edac7161e
Manish Hazra <64765035+Manish-Hazra@users.noreply.github.com>
2021-09-15 17:19:17 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -15,8 +15,52 @@ const handleRequest = (request, response) => {
         }
         response.end();
     });
+    fs.readFile('./assets/html/dashboard.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    fs.readFile('./assets/html/can1.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    fs.readFile('./assets/html/can2.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    fs.readFile('./assets/html/can3.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    fs.readFile('./assets/html/signup.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
 };
 
 http.createServer(handleRequest).listen(8000);
-console.log("Server on port 8000...);
-
+console.log("Server on port 8000...");
